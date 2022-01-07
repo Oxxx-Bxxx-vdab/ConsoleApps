faae48047da18d745b03b8d6fdb5879686b9b4ad
Olesj Bilous <olesj.bilous@vdabcampus.be>
2022-01-07 16:56:30 +0100

    something new

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello Branch.");
+            Console.WriteLine("Something new.");
         }
     }
 }
